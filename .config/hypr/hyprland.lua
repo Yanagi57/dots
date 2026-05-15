6cdedeaad8f13449c35b6719b653ebc44ac514dc
Yanagi57 <yanagisen@outlook.com>
2026-05-15 08:50:41 +0800

    Fix exit

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -178,7 +178,7 @@ local mainMod = "SUPER"
 hl.bind(mainMod .. " + Return",            hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + Q",                 hl.dsp.window.close())
 hl.bind(mainMod .. " + B",                 hl.dsp.exec_cmd(browser))
-hl.bind(mainMod .. " + SHIFT + Backspace", hl.dsp.exec_cmd("hyprctl dispatch exit"))
+hl.bind(mainMod .. " + SHIFT + Backspace", hl.dsp.exit())
 hl.bind(mainMod .. " + E",                 hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + F",                 hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + SHIFT + F",         hl.dsp.window.fullscreen())
