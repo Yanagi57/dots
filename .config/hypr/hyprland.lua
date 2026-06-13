ee573346a49125ea537afd87abefcb5a4c78fb99
Yanagi57 <yanagisen@outlook.com>
2026-06-13 11:22:06 +0800

    nvim: add panda

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -133,8 +133,9 @@ hl.config({
 
 hl.config({
     misc = {
-        force_default_wallpaper = 1,
-        disable_hyprland_logo   = true,
+        force_default_wallpaper     = 1,
+        disable_hyprland_logo       = true,
+        allow_session_lock_restore  = true,
     },
 })
 
